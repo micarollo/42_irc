0941047465a264ea59e3ae9428b6b020746211b0
arebelo <anagmrebelo@hotmail.com>
2023-11-04 22:57:57 +0100

    Organize imports and add try catch structure

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,6 +1,31 @@
+// Libraries
+#include <iostream>
+
+// Classes
+#include "Parser.hpp"
+#include "Server.hpp"
+
+// Macros
+
+void print_error(std::string msg)
+{
+	std::cout << msg << std::endl;
+}
+
 int main(int argc, char **argv)
 {
-	(void)argc;
-	(void)argv;
-	return (0);
+	try
+	{
+		Parser parser(argc, argv);
+		Server server(argv);
+
+		server.run();
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+		return (1);
+	}
+
+	return (1);
 }
