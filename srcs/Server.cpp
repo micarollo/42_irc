b32f7c25c6993c9c74cb400f5e11f7a05b0418c6
arebelo <anagmrebelo@hotmail.com>
2023-11-04 22:56:50 +0100

    Add password and port attr to class Server

--- srcs/Server.cpp
+++ srcs/Server.cpp
@@ -6,6 +6,13 @@ Server::Server(void)
 	return;
 }
 
+Server::Server(char **argv)
+{
+	_port = atoi(argv[1]);
+	_srvPassword = std::string(argv[2]);
+	return;
+}
+
 Server::Server(const Server &src)
 {
 	(void)src;
@@ -23,4 +30,10 @@ Server &Server::operator=(const Server &rhs)
 {
 	(void)rhs;
 	return *this;
+}
+
+// Methods
+void Server::run(void)
+{
+	return;
 }
