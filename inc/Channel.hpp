e131ae807e4dd287af79594a068d0288b058573f
arebelo <anagmrebelo@hotmail.com>
2023-11-05 14:25:42 +0100

    Add users and operators to Channel class

--- inc/Channel.hpp
+++ inc/Channel.hpp
@@ -1,14 +1,28 @@
 #ifndef __CHANNEL_HPP__
 #define __CHANNEL_HPP__
 
+// Libraries
+#include <map>
+
+// Classes
+#include "Client.hpp"
+
 class Channel
 {
 public:
+	// Constructors and Destructors
+	~Channel(void);
+
 private:
+	// Cannonical Form
 	Channel(void);
 	Channel(const Channel &src);
-	~Channel(void);
 	Channel &operator=(const Channel &rhs);
+
+	// Attributes
+	std::map<std::string, Client *> _users;
+	std::map<std::string, Client *> _operators;
+	int _userLimit;
 };
 
 #endif
