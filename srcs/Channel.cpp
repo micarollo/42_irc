ced82c63d471215fa16e1c0b386af8927f0ba880
Ana Rebelo <anagmrebelo@hotmail.com>
2023-11-30 17:31:48 +0100

    Add getters for Channel and attribute _key

--- srcs/Channel.cpp
+++ srcs/Channel.cpp
@@ -6,6 +6,15 @@ Channel::Channel(void)
 	return;
 }
 
+Channel::Channel(std::string name, Client &founderClient)
+{
+	_name = name,
+	_topic = "",
+	_users[founderClient.getNickName()] = &founderClient;
+	_operators[founderClient.getNickName()] = &founderClient;
+	return;
+}
+
 Channel::Channel(const Channel &src)
 {
 	(void)src;
@@ -23,4 +32,20 @@ Channel &Channel::operator=(const Channel &rhs)
 {
 	(void)rhs;
 	return *this;
+}
+
+// Getters
+std::string const &Channel::getName() const
+{
+	return _name;
+}
+
+std::map<std::string, Client *> const &Channel::getUsers() const
+{
+	return _users;
+}
+
+std::map<std::string, Client *> const &Channel::getOperators() const
+{
+	return _users;
 }
