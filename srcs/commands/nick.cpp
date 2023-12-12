2329869c8ffe5ba9dce6669314483b47048e878d
micarollo <rollomicaela@gmail.com>
2023-12-12 14:42:15 +0100

    update rpl welcome msg nick and user

--- srcs/commands/nick.cpp
+++ srcs/commands/nick.cpp
@@ -37,6 +37,8 @@ void Executor::nick()
 				_cmd->getClientExec()->setStatus(REGISTERED);
 				// RPL_WELCOME
 				_cmd->getClientExec()->sendMsg(RPL_WELCOME(_cmd->getClientExec()->getUserName(), "network", _cmd->getClientExec()->getNickName()));
+				_cmd->getClientExec()->sendMsg(RPL_YOURHOST(_cmd->getClientExec()->getUserName(), _srv->getName()));
+				_cmd->getClientExec()->sendMsg(RPL_MYINFO(_cmd->getClientExec()->getUserName(), _srv->getName()));
 			}
 			else
 			{
