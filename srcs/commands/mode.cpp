8eeed33df4ef7212edf42247dc6ba89a572e08ae
micarollo <rollomicaela@gmail.com>
2024-01-08 12:43:37 +0100

    error when no channels and no for

--- srcs/commands/mode.cpp
+++ srcs/commands/mode.cpp
@@ -8,45 +8,88 @@ void Executor::mode()
 
     if (_cmd->getClientExec()->getStatus() == REGISTERED)
     {
-        for (std::map<std::string, Channel *>::iterator it = channels.begin(); it != channels.end(); it++)
+        if (channels[_cmd->getParams()[0]])
         {
-            if (it->second->getName() == _cmd->getParams()[0])
+            if (_cmd->getParams().size() == 1 || (_cmd->getParams()[1][0] != '+' && _cmd->getParams()[1][0] != '-'))
             {
-                if (_cmd->getParams().size() == 1 || (_cmd->getParams()[1][0] != '+' && _cmd->getParams()[1][0] != '-'))
+                std::string modes = channels[_cmd->getParams()[0]]->getModes();
+                _cmd->getClientExec()->sendMsg(RPL_CHANNELMODEIS(_cmd->getClientExec()->getUserName(), channels[_cmd->getParams()[0]]->getName(), "", modes, ""));
+                _cmd->getClientExec()->sendMsg(RPL_CREATIONTIME(_cmd->getClientExec()->getUserName(), channels[_cmd->getParams()[0]]->getName(), getCurrentTime()));
+                return;
+            }
+            if (channels[_cmd->getParams()[0]]->isOperator(_cmd->getClientExec()->getNickName()))
+            {
+                std::map<std::string, std::string> modes = checkModes(_cmd->getParams()[1]);
+                if (modes["+"].size() > 0)
                 {
-                    std::string modes = it->second->getModes();
-                    _cmd->getClientExec()->sendMsg(RPL_CHANNELMODEIS(_cmd->getClientExec()->getUserName(), it->second->getName(), "", modes, ""));
-                    _cmd->getClientExec()->sendMsg(RPL_CREATIONTIME(_cmd->getClientExec()->getUserName(), it->second->getName(), getCurrentTime()));
-                    return;
+                    channels[_cmd->getParams()[0]]->addModes(modes["+"]);
+                    channels[_cmd->getParams()[0]]->sendMsg(RPL_CREATIONTIME(_cmd->getClientExec()->getUserName(), channels[_cmd->getParams()[0]]->getName(), getCurrentTime()));
+                    channels[_cmd->getParams()[0]]->sendMsg(RPL_CHANNELMODEIS(_cmd->getClientExec()->getUserName(), channels[_cmd->getParams()[0]]->getName(), "+", modes["+"], ""));
                 }
-                if (it->second->isOperator(_cmd->getClientExec()->getNickName()))
+                if (modes["-"].size() > 0)
                 {
-                    std::map<std::string, std::string> modes = checkModes(_cmd->getParams()[1]);
-                    if (modes["+"].size() > 0)
-                    {
-                        it->second->addModes(modes["+"]);
-                        it->second->sendMsg(RPL_CREATIONTIME(_cmd->getClientExec()->getUserName(), it->second->getName(), getCurrentTime()));
-                        it->second->sendMsg(RPL_CHANNELMODEIS(_cmd->getClientExec()->getUserName(), it->second->getName(), "+", modes["+"], ""));
-                    }
-                    if (modes["-"].size() > 0)
-                    {
-                        it->second->removeModes(modes["-"]);
-                        it->second->sendMsg(RPL_CREATIONTIME(_cmd->getClientExec()->getUserName(), it->second->getName(), getCurrentTime()));
-                        it->second->sendMsg(RPL_CHANNELMODEIS(_cmd->getClientExec()->getUserName(), it->second->getName(), "+", modes["-"], ""));
-                    }
-                }
-                else
-                {
-                    _cmd->getClientExec()->sendMsg(ERR_CHANOPRIVSNEEDED(_cmd->getClientExec()->getUserName(), it->second->getName()));
-                    return;
+                    channels[_cmd->getParams()[0]]->removeModes(modes["-"]);
+                    channels[_cmd->getParams()[0]]->sendMsg(RPL_CREATIONTIME(_cmd->getClientExec()->getUserName(), channels[_cmd->getParams()[0]]->getName(), getCurrentTime()));
+                    channels[_cmd->getParams()[0]]->sendMsg(RPL_CHANNELMODEIS(_cmd->getClientExec()->getUserName(), channels[_cmd->getParams()[0]]->getName(), "-", modes["-"], ""));
                 }
             }
             else
             {
-                _cmd->getClientExec()->sendMsg(ERR_NOSUCHCHANNEL(_cmd->getClientExec()->getUserName(), _cmd->getParams()[0]));
+                _cmd->getClientExec()->sendMsg(ERR_CHANOPRIVSNEEDED(_cmd->getClientExec()->getUserName(), channels[_cmd->getParams()[0]]->getName()));
                 return;
             }
         }
+        // }
+        // for (std::map<std::string, Channel *>::iterator it = channels.begin(); it != channels.end(); it++)
+        // {
+        //     if (it->second->getName() == _cmd->getParams()[0])
+        //     {
+        //         if (_cmd->getParams().size() == 1 || (_cmd->getParams()[1][0] != '+' && _cmd->getParams()[1][0] != '-'))
+        //         {
+        //             std::string modes = it->second->getModes();
+        //             _cmd->getClientExec()->sendMsg(RPL_CHANNELMODEIS(_cmd->getClientExec()->getUserName(), it->second->getName(), "", modes, ""));
+        //             _cmd->getClientExec()->sendMsg(RPL_CREATIONTIME(_cmd->getClientExec()->getUserName(), it->second->getName(), getCurrentTime()));
+        //             return;
+        //         }
+        //         if (it->second->isOperator(_cmd->getClientExec()->getNickName()))
+        //         {
+        //             std::map<std::string, std::string> modes = checkModes(_cmd->getParams()[1]);
+        //             if (modes["+"].size() > 0)
+        //             {
+        //                 it->second->addModes(modes["+"]);
+        //                 it->second->sendMsg(RPL_CREATIONTIME(_cmd->getClientExec()->getUserName(), it->second->getName(), getCurrentTime()));
+        //                 it->second->sendMsg(RPL_CHANNELMODEIS(_cmd->getClientExec()->getUserName(), it->second->getName(), "+", modes["+"], ""));
+        //             }
+        //             if (modes["-"].size() > 0)
+        //             {
+        //                 it->second->removeModes(modes["-"]);
+        //                 it->second->sendMsg(RPL_CREATIONTIME(_cmd->getClientExec()->getUserName(), it->second->getName(), getCurrentTime()));
+        //                 it->second->sendMsg(RPL_CHANNELMODEIS(_cmd->getClientExec()->getUserName(), it->second->getName(), "-", modes["-"], ""));
+        //             }
+        //         }
+        //         else
+        //         {
+        //             _cmd->getClientExec()->sendMsg(ERR_CHANOPRIVSNEEDED(_cmd->getClientExec()->getUserName(), it->second->getName()));
+        //             return;
+        //         }
+        //     }
+        //     else
+        //     {
+        //         _cmd->getClientExec()->sendMsg(ERR_NOSUCHCHANNEL(_cmd->getClientExec()->getUserName(), _cmd->getParams()[0]));
+        //         return;
+        //     }
+        // }
+        // }
+        else
+        {
+            _cmd->getClientExec()->sendMsg(ERR_NOSUCHCHANNEL(_cmd->getClientExec()->getUserName(), _cmd->getParams()[0]));
+            return;
+        }
+    }
+    else
+    {
+        _cmd->getClientExec()->sendMsg(ERR_NOTREGISTERED(_cmd->getClientExec()->getUserName()));
+        return;
     }
 }
 
